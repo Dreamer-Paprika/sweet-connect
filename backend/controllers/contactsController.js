8225bf5655802284fb5970c2dc20428b6c3c8517
Oseyijotham <vanclise277@gmail.com>
2024-09-27 14:19:23 +0100

    Update App

---
+++ backend/controllers/contactsController.js
@@ -0,0 +1,151 @@
+import { Contact } from "../models/contactsModel.js";
+import { User } from '../models/usersModel.js';
+// prettier-ignore
+import { contactValidation, favoriteValidation } from "../validations/validation.js";
+import { httpError } from "../helpers/httpError.js";
+import 'dotenv/config';
+const { ACCESS_TOKEN, PROJECT_ID } = process.env;
+import TheAuthAPI from "theauthapi";
+
+const theAuthAPI = new TheAuthAPI(ACCESS_TOKEN);
+
+const retrieveKey = async (req, res) => {
+  const { apiKey } = req.user;
+  res.json(apiKey);
+};
+
+const createAPIkey = async (req, res, next) => {
+  const { name, customMetaData, customAccountId } = req.body;
+  try {
+    console.log(req.body);
+    const key = await theAuthAPI.apiKeys.createKey({
+      projectId: PROJECT_ID,
+      customMetaData: { metadata_val: customMetaData },
+      customAccountId: customAccountId,
+      name: name,
+    });
+    console.log('Key created > ', key);
+    const { _id } = req.user;
+    await User.findByIdAndUpdate(_id, { apiKey: key.key });
+    res.json(key);
+  } catch (error) {
+    console.log("Couldn't make the key ", error);
+    next(error);
+  }
+};
+
+
+const createApiKey = async (req, res) => {
+   try {
+     const { data } = await axios.post(apiUrl + '/api-keys', apiKey, {
+       headers: {
+         ContentType: 'application/json',
+         'x-api-key': accessKey,
+       },
+     });
+     return data;
+   } catch (error) {
+     if (error.response) {
+       console.log(error.response.data);
+     } else {
+       // handle other errors
+     }
+   }
+};
+
+const getAllContacts = async (req, res) => {
+  //const { page = 1, limit = 20, favorite } = req.query;
+  //const query = favorite ? { favorite: true } : {};
+  //const result = await Contact.find(query)
+    //.skip((page - 1) * limit)
+  //.limit(parseInt(limit));
+  const { _id } = req.user
+  const contacts = await Contact.find({ owner: _id });
+  res.json(contacts);
+
+};
+
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findById(contactId);
+
+  if (!result) {
+    throw httpError(404, "Contact ID Not Found");
+  }
+
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  // Preventing lack of necessary data for contacts (check validations folder)
+  const { _id } = req.user;
+  const { error } = contactValidation.validate(req.body);
+
+  if (error) {
+    throw httpError(400, "missing required fields");
+  }
+
+  const result = await Contact.create({ ...req.body, owner:_id });
+
+  res.status(201).json(result);
+};
+const deleteContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndDelete(contactId);
+
+  if (!result) {
+    throw httpError(404);
+  }
+
+  res.json(contactId);
+};
+
+const updateContactById = async (req, res) => {
+  // Preventing lack of necessary data for contacts (check validations folder)
+  const { error } = contactValidation.validate(req.body);
+  if (error) {
+    throw httpError(400, "missing fields");
+  }
+
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+
+  if (!result) {
+    throw httpError(404);
+  }
+
+  res.json(result);
+};
+
+const updateStatusContact = async (req, res) => {
+  // Preventing lack of necessary data for favorite (check validations folder)
+  const { error } = favoriteValidation.validate(req.body);
+  if (error) {
+    throw httpError(400, "missing field favorite");
+  }
+
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+
+  if (!result) {
+    throw httpError(404);
+  }
+
+  res.json(result);
+};
+
+// prettier-ignore
+export {
+  getAllContacts,
+  getContactById,
+  addContact,
+  deleteContactById,
+  updateContactById,
+  updateStatusContact,
+  retrieveKey,
+  createAPIkey,
+};                                        
---
+++ src/redux/AppRedux/operations.jsx
@@ -0,0 +1,90 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+
+export const getApiKey = createAsyncThunk(
+  'contacts/getApiKey',
+  async ({
+          name,
+          customMetaData,
+          customAccountId
+  }, thunkAPI) => {
+    console.log({
+      name,
+      customMetaData,
+      customAccountId,
+    });
+    try {
+      const res = await axios.post('/contacts/key', {
+        name,
+        customMetaData,
+        customAccountId,
+      });
+      alert("API KEY CREATED, CHECK HOMEPAGE for DETAILS");
+      console.log(res.data);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const retrieveApiKey = createAsyncThunk(
+  'contacts/retrieveKey',
+  async (_, thunkAPI) => {
+    try {
+      const res = await axios.get('/contacts/retrieve');
+      console.log(res.data);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+        const response = await axios.get('/contacts');
+        //console.log (response.data);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async ({ name, phone }, thunkAPI) => {
+      const state = thunkAPI.getState();
+      const valKey = state.contacts.contacts.val;
+    try {
+      const response = await axios.post(`/contacts/${valKey}`, { name, phone });
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (taskId, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const valKey = state.contacts.contacts.val;
+    try {
+      const response = await axios.delete(`/contacts/${taskId}/${valKey}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);          
+
+                                          
